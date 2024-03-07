c1a1a007fc5482d4004d7598acbf28a4108ad3b6
Juan Pablo Chancay <jpcpol@gmail.com>
2024-03-07 13:19:51 -0300

    Users Page

--- airline/flights/admin.py
+++ airline/flights/admin.py
@@ -6,7 +6,9 @@ from .models import Flight, Airport, Passenger
 class FlightAdmin(admin.ModelAdmin):
     list_display =("id", "origin", "destination", "duration")
     
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("fligths",)
 
 admin.site.register(Airport)
 admin.site.register(Flight, FlightAdmin)
-admin.site.register(Passenger)
+admin.site.register(Passenger, PassengerAdmin)
